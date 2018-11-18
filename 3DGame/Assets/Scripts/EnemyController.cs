4b2969d1369661321caeffa00cd6cff6829cefbb
cilach <demadima1601@gmail.com>
2018-11-18 23:53:52 +0200

    3DGame v0.4

---
+++ 3DGame/Assets/Scripts/EnemyController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour {
+
+    public Transform playerPos;
+
+	void Start () {
+        playerPos = GameObject.Find("Player").transform;
+	}
+	
+
+	void Update () {
+        //Если получили координаты игрока ,поворачиваем енеми и перемещаем
+        if (playerPos)
+        {
+            transform.LookAt(playerPos);
+            transform.position += transform.forward * Time.deltaTime;
+        }
+	}
+}
---
+++ 3DGame/Assets/Scripts/Game.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Game : MonoBehaviour {
+
+    public GameObject[] spawn;
+    public GameObject enemy;
+
+    public int count=0;
+
+    public float spawnTime = 1f;
+
+	void Start () {
+        Transform spawns = GameObject.Find("Spawns").transform;
+        spawn = new GameObject[6];
+        //Заносим наши точки спама в массив
+        for (int i = 0; i < spawns.childCount; i++)
+            spawn[i] = spawns.FindChild(i.ToString()).gameObject;
+        StartCoroutine(Spawner());
+    }
+
+    IEnumerator Spawner(){
+        //Если объектов больше указаного количества, то изменяем время спавна
+        Transform enemys = GameObject.Find("EnemyClons").transform;
+        if (enemys.childCount > 2) { spawnTime = 2f; }
+
+        int i = Random.Range(0, 6);
+
+        //Создаем объект енеми  и переименовываем его
+        var myObject = Instantiate(enemy, spawn[i].transform.position, spawn[i].transform.rotation) ;
+        myObject.name = "Enemy" + count++;
+        //Указываем родителя
+        myObject.transform.parent = GameObject.Find("EnemyClons").transform;
+
+        //Функциия задержки
+        yield return new WaitForSeconds(spawnTime);
+        StartCoroutine(Spawner());
+         
+            
+    }
+}
