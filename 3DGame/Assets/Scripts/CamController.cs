fa0f46018e39b0af41cb81296c079c6d137f85bc
cilach <demadima1601@gmail.com>
2018-11-18 22:30:12 +0200

    3DGame v0.3

---
+++ 3DGame/Assets/Scripts/CamController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamController : MonoBehaviour {
+
+    public Transform player;
+
+    public float distance = 2f;
+    public float height = 2f;
+
+
+	void Start () {
+        //Присваиваем player текущую позицию объкта
+        player = GameObject.Find("Player").transform;
+        //Поворот по оси х
+        this.transform.eulerAngles = new Vector3(50, 0, 0);
+	}
+	
+	
+	void Update () {
+        //Позиция камеры
+        this.transform.position = new Vector3(player.position.x, player.position.y + height, player.position.z - distance);
+
+    }
+}
---
+++ 3DGame/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float horizontal;
+    public float vertical;
+    //Вектор движения
+    public Vector3 velocity;
+
+    public Animator anim;
+    public Camera cam;
+    public Rigidbody rb;
+    public GameObject mario;
+
+    
+
+	void Start () {
+        anim = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody>();
+        cam = GameObject.Find("Main Camera").GetComponent<Camera>();
+	}
+	
+
+	void Update () {
+        //Переменные ,которые отвечают за перемещение
+        vertical = Input.GetAxis("Vertical");
+        horizontal = Input.GetAxis("Horizontal");
+
+        rb.velocity = new Vector3(horizontal * 3, 0, vertical * 3);
+
+        //Вращение игрока по наведению мыши
+        RaycastHit hit;
+        //Создаем луч
+        Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+        //Столкновение луча
+        if (Physics.Raycast(ray, out hit)) {
+            transform.LookAt(new Vector3(hit.point.x, transform.position.y, hit.point.z));
+        }
+        // Для анимации
+        velocity = transform.InverseTransformDirection(rb.velocity);
+
+        anim.SetFloat("Vertical", velocity.z);
+        anim.SetFloat("Horizontal", velocity.x);
+
+	}
+}
